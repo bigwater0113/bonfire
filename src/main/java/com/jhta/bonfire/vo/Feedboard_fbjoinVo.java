1d514f2f0c80b74e2e077e5e1866e282b374bfb6
Daeun <Daeun@DESKTOP-ML1CJ5L>
2021-02-02 09:39:48 +0900

    여행자게시판 초기

---
+++ src/main/java/com/jhta/bonfire/vo/Feedboard_fbjoinVo.java
@@ -0,0 +1,40 @@
+package com.jhta.bonfire.vo;
+
+import java.sql.Date;
+
+public class Feedboard_fbjoinVo {
+  // feedboard_fbcomment_fbfiles_fbhits_fbrecommVo 
+		private int num;
+		private String id;
+		private String title;
+		private String content;
+		private int recommand;
+		private int hits;
+		private int scrap;
+		private int ispost;
+		private Date postdate;
+		private Date adddate;
+		private String cname;
+		
+		private int comidx;
+		private int comnum;
+		private String comid;
+		private String comcontent;
+		private Date comadddate;
+		
+		private int fidx;
+		private int fnum;
+		private String savefilename;
+		private String orgfilename;
+		
+		private int hnum;
+		private String hid;
+		
+		private int ridx;
+		private int rnum;
+		private int rid;
+		private int value;
+		private Date radddate;
+		
+		
+}

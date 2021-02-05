4c231d8ebb388eff0bba155b73cdcf8eea54b28e
euokyun <euokyun@gmail.com>
2021-02-05 17:03:38 +0900

    remove lombok vo

--- src/main/java/com/jhta/bonfire/vo/SbhitsVo.java
+++ src/main/java/com/jhta/bonfire/vo/SbhitsVo.java
@@ -6,9 +6,40 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class SbhitsVo {
     private int num;
     private String id;
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public SbhitsVo() {
+    }
+
+    public SbhitsVo(int num, String id) {
+        this.num = num;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " num='" + getNum() + "'" +
+            ", id='" + getId() + "'" +
+            "}";
+    }
+
 }

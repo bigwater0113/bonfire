93e8798e059cc134c691c5263a2ce2e10d8d8a75
bigwater0113 <xotnwnl@naver.com>
2021-02-10 17:44:10 +0900

    main 깡통

--- src/main/java/com/jhta/bonfire/controller/FeedController.java
+++ src/main/java/com/jhta/bonfire/controller/FeedController.java
@@ -18,17 +18,32 @@ public class FeedController {
 	@GetMapping(value="/@{feedId}")
 	public String feed(@PathVariable("feedId") String feedId,HttpSession session,Model model) {
 		String id=(String)session.getAttribute("id");
+		System.out.println(id);
 		System.out.println(feedId);
-		session.setAttribute("feedId",feedId);
 		HashMap<String, Object> map=service.feedCheck(feedId);
 		String feedRole=service.roleCheck(feedId);
-		session.setAttribute("feedRole", feedRole);
 		if(id==null) {
 			model.addAttribute("errorMsg","로그인후 이용가능합니다");
 			return ".home";
 		}else {
 			if(map!=null) {
-				return ".feed";
+				if(feedRole.equals("ROLE_MEMBER")) {
+					if(id.equals(feedId) || id.equals("admin")) {
+						session.setAttribute("feedId",feedId);
+						session.setAttribute("feedRole", feedRole);
+						return ".feed";
+					}else {
+						model.addAttribute("errorMsg","일반회원피드입니다. 접근불가.");
+						return ".home";
+					}
+				}else if(feedRole.equals("ROLE_AUTHOR")) {
+					session.setAttribute("feedId",feedId);
+					session.setAttribute("feedRole", feedRole);
+					return ".feed";
+				}else {
+					model.addAttribute("errorMsg","관리자 피드입니다. 접근불가.");
+					return ".home";
+				}
 			}else {
 				model.addAttribute("errorMsg","해당 피드가 존재하지 않습니다.");
 				return ".home";

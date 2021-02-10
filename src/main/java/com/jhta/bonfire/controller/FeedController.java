60938838eb5802819cacc234d60f6609dc42076f
bigwater0113 <xotnwnl@naver.com>
2021-02-10 18:25:06 +0900

    피드 접근 처리 CommonUtil

--- src/main/java/com/jhta/bonfire/controller/FeedController.java
+++ src/main/java/com/jhta/bonfire/controller/FeedController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.jhta.bonfire.service.FeedService;
+import com.jhta.bonfire.util.CommonUtil;
 @Controller
 public class FeedController {
 	@Autowired FeedService service;
@@ -22,32 +23,14 @@ public class FeedController {
 		System.out.println(feedId);
 		HashMap<String, Object> map=service.feedCheck(feedId);
 		String feedRole=service.roleCheck(feedId);
-		if(id==null) {
-			model.addAttribute("errorMsg","로그인후 이용가능합니다");
+		Boolean pageBlock=CommonUtil.pageBlock(id, feedId, feedRole, map);
+		if(pageBlock) {
+			model.addAttribute("errorMsg","접근 불가");
 			return ".home";
 		}else {
-			if(map!=null) {
-				if(feedRole.equals("ROLE_MEMBER")) {
-					if(id.equals(feedId) || id.equals("admin")) {
-						session.setAttribute("feedId",feedId);
-						session.setAttribute("feedRole", feedRole);
-						return ".feed";
-					}else {
-						model.addAttribute("errorMsg","일반회원피드입니다. 접근불가.");
-						return ".home";
-					}
-				}else if(feedRole.equals("ROLE_AUTHOR")) {
-					session.setAttribute("feedId",feedId);
-					session.setAttribute("feedRole", feedRole);
-					return ".feed";
-				}else {
-					model.addAttribute("errorMsg","관리자 피드입니다. 접근불가.");
-					return ".home";
-				}
-			}else {
-				model.addAttribute("errorMsg","해당 피드가 존재하지 않습니다.");
-				return ".home";
-			}
+			model.addAttribute("feedId",feedId);
+			model.addAttribute("feedRole",feedRole);
+			return ".feed";
 		}
 	}
 }

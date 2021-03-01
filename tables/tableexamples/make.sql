920fcce96288dd53dc700f74456264576753e828
euokyun <euokyun@gmail.com>
2021-03-01 15:15:54 +0900

    tableexample

--- tables/tableexamples/make.sql
+++ tables/tableexamples/make.sql
@@ -21,3 +21,39 @@ insert into cats values('작가추천', 'subscriberboard');
 insert into cats values('자유', 'subscriberboard');
 insert into cats values('팁', 'subscriberboard');
 -- localmap
+-- (.*)	(.*)	(.*)	(.*)	(.*)	(.*)	(.*)	(.*)	(.*)	(.*)	(.*)	(.*)	(.*)
+-- insert into localmap values('$1','$2','$3','$4','$5','$6','$7','$8','$9','$10','$11','$12', '$13');
+insert into localmap values('Feature','Point','128.419565730807','34.8860381156733','경남 통영시 광도면 죽림리 1570-7','FD6','음식점','음식점 > 한식 > 한정식','26836607','055-645-2623','밥','http://place.map.kakao.com/26836607', '경남 통영시 광도면 죽림5로 34');
+insert into localmap values('Feature','Point','127.10821439095204','37.51494203901618','서울 송파구 방이동 28-2','CE7','카페','음식점 > 카페','1856257072','02-422-4906','밥','http://place.map.kakao.com/1856257072', '서울 송파구 올림픽로30길 9');
+insert into localmap values('Feature','Point','126.71056629578','35.9600519848156','전북 군산시 수송동 794-7','FD6','음식점','음식점 > 한식','1809095446','063-461-5461','밥','http://place.map.kakao.com/1809095446', '전북 군산시 축동로 7');
+insert into localmap values('Feature','Point','127.06326369443','37.2111231682014','경기 화성시 반송동 4-1','FD6','음식점','음식점 > 한식','1141246750','031-613-2216','밥','http://place.map.kakao.com/1141246750', '경기 화성시 동탄반송1길 40');
+insert into localmap values('Feature','Point','128.605209281502','36.000214780129284','경북 칠곡군 동명면 기성리 661','AD5','숙박','여행 > 숙박 > 여관,모텔','2144174209','054-975-7117','팔공산AA무인텔','http://place.map.kakao.com/2144174209', '경북 칠곡군 동명면 팔공산로 143');
+insert into localmap values('Feature','Point','126.644249195952','37.5282988097563','인천 서구 청라동 182-10','CE7','카페','음식점 > 카페','27418103','032-562-0369','AA COFFEE','http://place.map.kakao.com/27418103', '인천 서구 청라커낼로223번길 3');
+insert into localmap values('Feature','Point','126.83650369902168','33.53833652360498','제주특별자치도 제주시 구좌읍 한동리 8-8','AD5','숙박','여행 > 숙박 > 게스트하우스','26940782','','Jeju in aA','http://place.map.kakao.com/26940782', '제주특별자치도 제주시 구좌읍 계룡길 26-20');
+insert into localmap values('Feature','Point','128.721586528265','34.915975840055','경남 거제시 장목면 외포리 1254-10','CT1','문화시설','문화,예술 > 문화시설 > 미술관','18038620','055-687-8769','갤러리AA','http://place.map.kakao.com/18038620', '경남 거제시 장목면 옥포대첩로 536-2');
+insert into localmap values('Feature','Point','127.041847266747','37.5478635351506','서울 성동구 성수동1가 685-408','CE7','카페','음식점 > 카페 > 커피전문점','1812485148','','COㅍㅍEE','http://place.map.kakao.com/1812485148', '서울 성동구 서울숲6길 2');
+insert into localmap values('Feature','Point','127.054941204558','37.2399266145511','경기 수원시 영통구 망포동 382-3','FD6','음식점','음식점 > 술집 > 호프,요리주점','499249876','031-206-8839','ee맛','http://place.map.kakao.com/499249876', '경기 수원시 영통구 태장로 81');
+insert into localmap values('Feature','Point','128.893892952058','35.2280367365919','경남 김해시 부원동 171-23','FD6','음식점','음식점 > 한식','1715883326','055-312-2402','밥','http://place.map.kakao.com/1715883326', '경남 김해시 활천로5번길 1');
+insert into localmap values('Feature','Point','126.487827287935','33.4894749080095','제주특별자치도 제주시 연동 251-76','FD6','음식점','음식점 > 분식 > 김밥천국','617700739','064-744-0662','김밥천국 신제주점','http://place.map.kakao.com/617700739', '제주특별자치도 제주시 제원1길 14');
+-- tripplan
+-- (.*)	(.*)	(.*)	(.*)
+-- insert into tripplan values('$1','$2','$3','$4');
+insert into tripplan values('2','asdf','1812485148','0');
+insert into tripplan values('2','asdf','499249876','1');
+insert into tripplan values('3','asdf','1856257072','0');
+insert into tripplan values('3','asdf','1809095446','1');
+insert into tripplan values('3','asdf','1715883326','2');
+insert into tripplan values('4','asdf','1856257072','0');
+insert into tripplan values('4','asdf','1809095446','1');
+insert into tripplan values('4','asdf','1715883326','2');
+insert into tripplan values('4','asdf','21326892','3');
+insert into tripplan values('5','asdf','27418103','0');
+insert into tripplan values('5','asdf','2144174209','1');
+insert into tripplan values('6','asdf','1856257072','0');
+insert into tripplan values('6','asdf','1809095446','1');
+insert into tripplan values('6','asdf','1715883326','2');
+insert into tripplan values('7','asdf','27418103','0');
+insert into tripplan values('7','asdf','26940782','1');
+insert into tripplan values('8','asdf','1856257072','0');
+insert into tripplan values('8','asdf','1809095446','1');
+insert into tripplan values('8','asdf','1715883326','2');
